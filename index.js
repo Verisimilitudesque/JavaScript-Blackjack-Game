6415a69eb4af2aa1558c327fdb29b6ad84ffe849
Jon <140838296+Verisimilitudesque@users.noreply.github.com>
2025-04-22 10:20:10 -0600

    Added finishing touches
    Added random card generator function, player details, etc.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-// Card operators 
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
+let player = {
+    name: "Per",
+    chips: 145
+}
+
 //Card array
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive  = true
+let isAlive  = false
 let message = ""
 // Stores Blackjack dealer message
 let messageEL = document.getElementById("message-el")
@@ -13,15 +15,32 @@ let messageEL = document.getElementById("message-el")
 let sumEL = document.getElementById("sum-el")
 // Stores cards
 let cardsEL = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
 
+playerEl.textContent = player.name + ": $" + player.chips
+
+//Randomizes cards
 function getRandomCard() {
 
+    let cardsum =  Math.floor( Math.random() * 13 ) + 1
 
-
+    if (cardsum > 10) {
+        return 10
+    } else if (cardsum == 1) {
+        return 11
+    } else {
+        return cardsum
+    }
+   
 }
-
+// Initiates game
 function startGame() {
 
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
 
 }
@@ -49,15 +68,19 @@ function renderGame() {
     messageEL.textContent = message
 }
 
+// Allows player to draw new card
 function newCard() {
-    
-    let card = getRandomCard()
-    sum += card
-    cards.push(card)
-    console.log(cards)
-    renderGame()
+//Checks if player has too many cards
+    if (isAlive === true && hasBlackJack === false ) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        console.log(cards)
+        renderGame()
+    }
 
 }
+
 
 
 
